9fba1999fe240af96b500d637186cb5a91f1c1fe
Muradyan-Narine <narine.muradyan@mail.ru>
2023-07-30 13:13:36 +0300

    doctor_search

--- OneHealth-common/src/main/java/com/example/onehealthcommon/repository/DoctorRepository.java
+++ OneHealth-common/src/main/java/com/example/onehealthcommon/repository/DoctorRepository.java
@@ -1,12 +1,16 @@
 package com.example.onehealthcommon.repository;
+
 import com.example.onehealthcommon.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+public interface DoctorRepository extends JpaRepository<Doctor, Integer>,
+        QuerydslPredicateExecutor<Doctor> {
     Optional<Doctor> findByEmail(String email);
+
     List<Doctor> findBySurnameContaining(String key);
 
 }
